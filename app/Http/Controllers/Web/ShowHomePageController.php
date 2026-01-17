5a5d3072a8773d0822dffcf05dd914bf821e79b5
João Ramajo <joaoramajo744@gmail.com>
2026-01-17 02:03:46 -0300

    feat: adiciona seção de comentários recentes na home page1

--- app/Http/Controllers/Web/ShowHomePageController.php
+++ app/Http/Controllers/Web/ShowHomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,6 +11,11 @@ class ShowHomePageController extends Controller
 {
     public function __invoke(): View
     {
-        return view('home');
+        $recentComments = Comment::with(['snack', 'user'])
+            ->latest()
+            ->take(3)
+            ->get();
+
+        return view('home', compact('recentComments'));
     }
 }
